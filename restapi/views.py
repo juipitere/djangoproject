1c804e34eb7fa86db5f31eb31adf60837370dcd9
juipitere <69297032+juipitere@users.noreply.github.com>
2020-10-08 13:14:07 +0900

    Update views.py

--- restapi/views.py
+++ restapi/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+  return HttpResponse("Hello, Django!")
+
+def taskstring(request):
+  result = 'Rest API stinrg!'
+  return HttpResponse(result, content_type="text/plain")
